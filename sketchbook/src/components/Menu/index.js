d545631dac9095830757626eb1012d2bc2cb75b1
Nishan9t <pandeynisha9t@gmail.com>
2023-10-23 10:52:00 +0530

    classname library is used to enable active effect on menu items and now menu and toolbar is connected with redux

--- sketchbook/src/components/Menu/index.js
+++ sketchbook/src/components/Menu/index.js
@@ -3,7 +3,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEraser, faFileArrowDown, faPencil, faRotateLeft, faRotateRight } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './index.module.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch,useSelector } from 'react-redux';
+
+import cx from 'classnames';
 
 import { MENU_ITEMS } from '@/constants';
 import { menuItemClick,actionItemClick } from '@/slice/menuSlice';
@@ -11,6 +13,9 @@ import { menuItemClick,actionItemClick } from '@/slice/menuSlice';
 const Menu = ()=>{
 
     const dispatch=useDispatch();
+
+    const activeMenuItem = useSelector(state => state.menu.activeMenuItem)
+
 
     const handleMenuClick=(itemName)=>{
 
@@ -20,10 +25,10 @@ const Menu = ()=>{
 
     return(
         <div className={styles.menuContainer}>
-        <div className={styles.iconWrapper} onClick={()=>handleMenuClick(MENU_ITEMS.PENCIL)}>
+        <div className={cx(styles.iconWrapper , {[styles.active] : activeMenuItem === MENU_ITEMS.PENCIL})} onClick={()=>handleMenuClick(MENU_ITEMS.PENCIL)}>
                 <FontAwesomeIcon icon={faPencil} className={styles.icon} />
             </div>
-            <div className={styles.iconWrapper} onClick={()=>handleMenuClick(MENU_ITEMS.ERASER)}>
+            <div className={cx(styles.iconWrapper , {[styles.active] : activeMenuItem === MENU_ITEMS.ERASER})} onClick={()=>handleMenuClick(MENU_ITEMS.ERASER)}>
                 <FontAwesomeIcon icon={faEraser} className={styles.icon} />
             </div>
             <div className={styles.iconWrapper}>

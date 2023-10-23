b7201d5851ac110bb141142670b7e6c07b2296fc
Nishan9t <pandeynisha9t@gmail.com>
2023-10-23 10:24:24 +0530

    redux store is created and menuBox is used for current menuItem

--- sketchbook/src/components/Menu/index.js
+++ sketchbook/src/components/Menu/index.js
@@ -3,15 +3,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEraser, faFileArrowDown, faPencil, faRotateLeft, faRotateRight } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './index.module.css';
+import { useDispatch } from 'react-redux';
+
+import { MENU_ITEMS } from '@/constants';
+import { menuItemClick,actionItemClick } from '@/slice/menuSlice';
 
 const Menu = ()=>{
+
+    const dispatch=useDispatch();
+
+    const handleMenuClick=(itemName)=>{
+
+        dispatch(menuItemClick(itemName))
+
+    }
+
     return(
         <div className={styles.menuContainer}>
         <div className={styles.iconWrapper}>
-                <FontAwesomeIcon icon={faPencil} className={styles.icon}/>
+                <FontAwesomeIcon icon={faPencil} className={styles.icon} onClick={()=>handleMenuClick(MENU_ITEMS.PENCIL)}/>
             </div>
             <div className={styles.iconWrapper}>
-                <FontAwesomeIcon icon={faEraser} className={styles.icon}/>
+                <FontAwesomeIcon icon={faEraser} className={styles.icon} onClick={()=>handleMenuClick(MENU_ITEMS.ERASER)}/>
             </div>
             <div className={styles.iconWrapper}>
                 <FontAwesomeIcon icon={faRotateLeft} className={styles.icon}/>
